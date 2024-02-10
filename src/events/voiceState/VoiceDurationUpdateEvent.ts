90618387d03931a8a18b494766b9e8028bf32a1c
Gra <grace.bulckens@gmail.com>
2024-02-10 11:52:10 +0100

    Added better voice log

--- src/events/voiceState/VoiceDurationUpdateEvent.ts
+++ src/events/voiceState/VoiceDurationUpdateEvent.ts
@@ -5,84 +5,62 @@ import DiscordClient from '../../client/client';
 import { Info } from '../../utils/types';
 import { voice_stats } from '@prisma/client';
 
-enum types {
-  DEAF = 'selfDeaf',
-  MUTE = 'selfMute',
-  VIDEO = 'selfVideo',
-  MEMBER_UPDATE_DEAF = 'serverDeaf',
-  MEMBER_UPDATE_MUTE = 'serverMute',
-  STREAMING = 'streaming',
-}
 export default class VoiceDurationUpdateEvent extends BaseEvent {
-  voicestate: VoiceState;
   constructor() {
     super('voiceStateUpdate');
   }
 
   async run(client: DiscordClient, oldState: VoiceState, newState: VoiceState) {
-    const voiceUsers = client.voiceUsers;
     const date = new Date();
-    this.voicestate = newState;
+    const voiceUsers = client.voiceUsers;
+
     const userInfo: Info = {
       guild: newState.guild,
       member: newState.member!,
       channel: newState.channel!,
     };
 
+    // Function to handle voice user logging
+    const handleVoiceUserLog = async (memberId: string) => {
+      const index = voiceUsers.findIndex(voiceUser => voiceUser.member_id === memberId);
+      if (index !== -1) {
+        const voiceUser = voiceUsers[index];
+        voiceUser.ended_on = date;
+        await client.dataSource.voiceStats.create({
+          data: voiceUser as voice_stats,
+        });
+        voiceUsers.splice(index, 1);
+      }
+    };
+
     // User joined a voice channel
-    if (oldState.channel === null && newState.channel !== null) {
-      const GuildStatsLog: Partial<voice_stats> = {
+    if (!oldState.channel && newState.channel) {
+      voiceUsers.push({
         guild_id: newState.guild.id,
         member_id: newState.member!.id,
         channel_id: newState.channel!.id,
         type: 'VOICE',
         issued_on: date,
-      };
-
-      voiceUsers.push(GuildStatsLog);
+      });
       return;
     }
 
     // User left a voice channel
-    if (oldState.channel !== null && newState.channel === null) {
-      for (let i = voiceUsers.length - 1; i >= 0; i--) {
-        const voiceUser = voiceUsers[i];
-        if (voiceUser.member_id === userInfo.member.id) {
-          voiceUser.ended_on = date;
-          await client.dataSource.voiceStats.create({
-            data: voiceUser as voice_stats,
-          });
-          voiceUsers.splice(i, 1);
-        }
-      }
+    if (oldState.channel && !newState.channel) {
+      await handleVoiceUserLog(userInfo.member.id);
       return;
     }
 
     // User changed voice channel
-    if (oldState.channel !== null && newState.channel !== null) {
-      if (oldState.channel.id !== newState.channel.id) {
-        for (let i = voiceUsers.length - 1; i >= 0; i--) {
-          const voiceUser = voiceUsers[i];
-          if (voiceUser.member_id === userInfo.member.id) {
-            voiceUser.ended_on = date;
-            await client.dataSource.voiceStats.create({
-              data: voiceUser as voice_stats,
-            });
-            voiceUsers.splice(i, 1);
-          }
-        }
-
-        const GuildStatsLog: Partial<voice_stats> = {
-          guild_id: newState.guild.id,
-          member_id: newState.member!.id,
-          channel_id: newState.channel!.id,
-          type: 'VOICE',
-          issued_on: date,
-        };
-
-        voiceUsers.push(GuildStatsLog);
-        return;
-      }
+    if (oldState.channel && newState.channel && oldState.channel.id !== newState.channel.id) {
+      await handleVoiceUserLog(userInfo.member.id);
+      voiceUsers.push({
+        guild_id: newState.guild.id,
+        member_id: newState.member!.id,
+        channel_id: newState.channel!.id,
+        type: 'VOICE',
+        issued_on: date,
+      });
     }
   }
 }
