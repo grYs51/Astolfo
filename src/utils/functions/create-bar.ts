1780bd1b929a24dace2b97bee37fa2cfdf993019
Gra <grace.bulckens@gmail.com>
2024-12-24 14:36:06 +0100

    improve progress bar display

--- src/utils/functions/create-bar.ts
+++ src/utils/functions/create-bar.ts
@@ -2,5 +2,5 @@ export const createBar = (current: number, max: number, size = 15) => {
   const percentage = Math.round((current / max) * 100);
   const bar = Math.round((size * percentage) / 100);
   const empty = size - bar;
-  return `[${'='.repeat(bar)}${' '.repeat(empty)}] ${percentage}%`;
+  return `${'█'.repeat(bar)}${'░'.repeat(empty)}    ${percentage}%`;
 };
