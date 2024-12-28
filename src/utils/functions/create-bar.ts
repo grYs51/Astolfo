7508884f4df9cc97eedc99d16381e832922a53e2
Gra <grace.bulckens@gmail.com>
2024-12-28 15:07:24 +0100

    Fix create bar not negative

--- src/utils/functions/create-bar.ts
+++ src/utils/functions/create-bar.ts
@@ -1,5 +1,10 @@
 export const createBar = (current: number, max: number, size = 15) => {
-  const percentage = Math.round((current / max) * 100);
+  // Ensure values are not negative
+  current = Math.max(0, current);
+  max = Math.max(0, max);
+  size = Math.max(0, size);
+
+  const percentage = max === 0 ? 0 : Math.round((current / max) * 100);
   const bar = Math.round((size * percentage) / 100);
   const empty = size - bar;
   return `${'█'.repeat(bar)}${'░'.repeat(empty)}    ${percentage}%`;
