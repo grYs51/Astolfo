06fecc60d7867439c988c643edab21f319d5480f
Gra <grace.bulckens@gmail.com>
2024-12-30 16:21:23 +0100

    Add components library with Tailwind CSS integration and Storybook configuration

--- apps/web/src/app/app.component.ts
+++ apps/web/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { ComponentsComponent } from '@nx-stolfo/components';
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, ComponentsComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
