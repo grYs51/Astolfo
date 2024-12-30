bbccd4f428783417fd10e580d714b6f332dc42b9
kevin hauman <hauman.kevin@hotmail.com>
2024-12-30 17:22:34 +0100

    current leaderboard doesn't deduct inactive stats

--- apps/bot/src/utils/functions/leaderboard/currentLeaderboard.ts
+++ apps/bot/src/utils/functions/leaderboard/currentLeaderboard.ts
@@ -1,23 +1,41 @@
 import { voice_stats } from '@prisma/client';
-import { getVoiceStatsType } from './leaderboard';
+import {
+  Leaderboard,
+  getLeaderboardType,
+  getVoiceStatsType,
+} from './leaderboard';
 import { VOICE_TYPE } from '../../handlers/vc';
-
-const voiceTypesToCheck = [
-  VOICE_TYPE.VOICE,
-  VOICE_TYPE.MUTED,
-  VOICE_TYPE.DEAF,
-  VOICE_TYPE.SERVER_DEAF,
-  VOICE_TYPE.SERVER_MUTED,
-];
 
 export const getCurrentVoiceStats: getVoiceStatsType = (client, guildId) => {
   const stats = client.voiceUsers
-    .filter(
-      (x) =>
-        x.guild_id === guildId &&
-        voiceTypesToCheck.includes(x.type as VOICE_TYPE)
-    )
+    .filter((x) => x.guild_id === guildId && x.type === VOICE_TYPE.VOICE)
     .map((x) => ({ ...x, ended_on: new Date() })) as voice_stats[];
 
   return Promise.resolve(stats);
+};
+
+export const getCurrentLeaderboard: getLeaderboardType = (members, stats) => {
+  const leaderboard = stats.reduce((acc, stat) => {
+
+    const activeTime = stat.ended_on.getTime() - stat.issued_on.getTime();
+
+    const memberStat = acc.find((x) => x.id === stat.member_id);
+
+    if (memberStat) {
+      memberStat.count += activeTime;
+    } else {
+      const member = members.find((m) => m.id === stat.member_id);
+      if (member) {
+        acc.push({
+          id: stat.member_id,
+          count: activeTime,
+          name: member.displayName ?? member.user.username,
+        });
+      }
+    }
+
+    return acc;
+  }, [] as Leaderboard[]);
+
+  return leaderboard;
 };
