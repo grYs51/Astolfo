d595c33b2281d1dcf6d3ee87466ff90b8c61c3ba
Gra <grace.bulckens@gmail.com>
2025-01-03 23:51:01 +0100

    Add Discord authentication routes and session management

---
+++ apps/bot/src/api/@types/express.d.ts
@@ -0,0 +1,44 @@
+import { ValidateFunction } from 'ajv';
+import pino from 'pino';
+import { Db } from '../db';
+
+declare global {
+    namespace Express {
+        interface User extends LoggedUser {
+            id: string;
+            access_token: string;
+            refresh_token: string;
+        }
+        interface Request {
+            /**
+             * The request id.
+             * In case request contains X-Request-Id header, uses its value instead. (e.g. heroku)
+             */
+            id: string;
+
+            /**
+             * The Database.
+             */
+            db: Db;
+
+            /**
+             * The logger bound to the request.
+             */
+            log: pino.Logger;
+
+            /**
+             * Validate and return a request part.
+             */
+            validate<T>(
+                validate: ValidateFunction<T>,
+                part?: 'body' | 'query' | 'params'
+            ): T;
+        }
+    }
+}
+
+declare module 'express-serve-static-core' {
+    interface Request {
+        user?: any; // Adjust the type of 'user' as needed
+    }
+}
---
+++ apps/bot/src/api/routes/discord/index.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import { discordAuth, getStatus, redirect } from './handlers';
+import { isAuthenticated } from '../../utils.ts/middleware/isAuthenticated';
+import passport from 'passport';
+
+export default (router: Router) => {
+  router.route('/auth/status').get(isAuthenticated, getStatus);
+  router.route('/auth/login').get(passport.authenticate('discord'), discordAuth);
+  router.route('/auth/redirect').get(passport.authenticate('discord'), redirect);
+};
