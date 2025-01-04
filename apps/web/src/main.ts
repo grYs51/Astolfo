ad5a2b0ab37bfb137eb300ebe238859a36e32a0b
Gra <grace.bulckens@gmail.com>
2025-01-04 01:17:51 +0100

    Add auth library with profile store and integrate with Angular application

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -1,7 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { config } from './app/app.config.browser';
 
-bootstrapApplication(AppComponent, appConfig).catch((err) =>
+bootstrapApplication(AppComponent, config).catch((err) =>
   console.error(err)
 );
